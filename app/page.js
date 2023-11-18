dd735108a1b29f322e8d69d85a349b8356856518
Emkauno <emkauno@gmail.com>
2023-11-18 06:58:04 -0300

    initial layout

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Products from '@/components/Products'
+import Contact from '@/components/Contact'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>test</h1>
+      <Hero/>
+      <About/>
+      <Products/>
+      <Contact/>
     </main>
   );
 }
