855e6ec2c96bb3a6b402fb3cf275cf5aef13b1af
Emkauno <emkauno@gmail.com>
2024-01-26 02:29:09 -0300

    styling base

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,16 @@
+'use client'
+const { FlexContainer } = require("@/styles/Containers.styles")
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <FlexContainer $minHeight="calc(100vh - 212px)"  $padding="32px 0" initial={{y:20,opacity: 0}} animate={{y:0,opacity: 1}} transition={{duration: .3}} $gap="32px">
+      <h1>wena</h1>
+      <h2>hola</h2>
+      <h3>hola</h3>
+      <h4>hola</h4>
+      <h5>hola</h5>
+      <h6>hola</h6>
+    </FlexContainer>
   )
 }
 
