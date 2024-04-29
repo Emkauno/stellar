f891983ade31a9082a4f53c007a0e30886feae0d
Emkauno <emkauno@gmail.com>
2024-04-29 14:58:35 -0400

    update media query

--- styles/SmokeBgStyles.js
+++ styles/SmokeBgStyles.js
@@ -2,15 +2,18 @@ import styled from 'styled-components'
 
 export const SmokeContainer = styled.div`
   width: 100vw;
-  height: 120vh;
+  height: 100vh;
   position: fixed;
   z-index: -3;
   opacity: 0.4;
   filter: invert(1);
+  @media(max-width: 500px){
+    display: none;
+  }
 `
 export const WhiteBg = styled.div`
   width: 100vw;
-  height: 120vh; 
+  height: 100vh; 
   position: fixed;
   z-index: -4;
   background: #fcfcf7;
