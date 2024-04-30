0e683245fd9a96c0878bce8b6d4742445c4609c0
Emkauno <emkauno@gmail.com>
2024-04-29 20:41:26 -0400

    updating getData function

--- app/page.js
+++ app/page.js
@@ -1,31 +1,33 @@
 "use client";
 
-import { use, useState } from "react";
+import { use, useState, useEffect} from "react";
 import styles from "./page.module.css";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Products from "@/components/Products";
 import Contact from "@/components/Contact";
-import { AnimatePresence } from "framer-motion";
 import Commerce from "@chec/commerce.js";
 import SmokeBg from "@/components/SmokeBg";
 import WhiteBackground from "@/components/WhiteBackground";
 
 export default function Home() {
-  const [smokeBackground, setSmokeBackground] = useState(false)
   const commerce = new Commerce(process.env.NEXT_PUBLIC_CHEC_PUBLIC_KEY);
+  const [ products, setProducts ] = useState([])
 
   const getData = async () => {
     try {
       const data = await commerce.products.list();
-      return data;
+      setProducts(data)
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
-  const products = use(getData());
-
+  useEffect(() => {
+    getData()
+  }, [])
+  
+console.log(products)
   return (
     <>
       {products ? (
