e8cd86d81180b33592c006443ec2129b0b4401eb
emkauno@gmail.com <emkauno@gmail.com>
2024-07-03 17:24:02 -0400

    update copyright

--- styles/FooterStyles.js
+++ styles/FooterStyles.js
@@ -45,6 +45,7 @@ export const FooterCopyright = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  text-align: center;
   p {
     font-size: 12px;
     font-weight: 500;
