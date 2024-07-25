17225afc0771b8735d98ec47984f87675b015f11
emkauno@gmail.com <emkauno@gmail.com>
2024-07-24 21:02:17 -0400

    fixing typo

--- components/Contact.js
+++ components/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
           ¿Tienes preguntas, comentarios o necesitas asesoramiento? ¡Estamos aquí para ayudarte en todo lo que necesites! No dudes en contactarnos a través de WhatsApp para recibir una atención personalizada y resolver cualquier inquietud que tengas.
         </p>
         <p>
-          Para realizar una compra, envíanos un mensaje por WhatsApp o un correo electrónico a <strong>ventas@stellar.tienda.cl</strong> <br/> A partir de ahí finalizamos con boleta o factura y organizaremos el envío.
+          Para realizar una compra, envíanos un mensaje por WhatsApp o un correo electrónico a <strong>ventas@stellartienda.cl</strong> <br/> A partir de ahí finalizamos con boleta o factura y organizaremos el envío.
 
         </p>
         <a href="https://wa.me/56983269643?text=Hola,%20me%20interesa%20un%20producto%20de%20Stellar%20Tienda">Contactar Ahora
